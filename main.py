4ac6dcb0ad7cd9e3f32017af6a626011a403e0b4
Nicholas <glicken@gmail.com>
2025-02-26 07:57:25 -0800

    added lines to PDF

--- main.py
+++ main.py
@@ -14,6 +14,9 @@ for i, row in df.iterrows():
     pdf.set_text_color(0, 100, 254)
     pdf.cell(w=0, h=12, txt=row["Topic"], border=0, ln=1, align="L", fill=False, link="")
     pdf.line(x1=10, y1=21, x2=200, y2=21)
+    pdf.set_line_width(0.5)
+    pdf.set_draw_color(0, 100, 254)
+    pdf.set_fill_color(200, 220, 255)
 
     # Set footer
     pdf.ln(265)
@@ -29,6 +32,7 @@ for i, row in df.iterrows():
         pdf.set_font("Arial", style="I", size=8)
         pdf.set_text_color(100, 100, 100)
         pdf.cell(w=0, h=12, txt=row["Topic"], border=0, ln=1, align="R")
+        
     
 
 
